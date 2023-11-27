ccbba7398549c3a48bac4e95ac6ac5bae5994a55
Ankit kumar <iamankitm05@gmail.com>
2023-11-27 14:57:48 +0530

    step-8

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -60,10 +60,20 @@ class _LoginViewState extends State<LoginView> {
                   email: email,
                   password: password,
                 );
-                Navigator.of(context).pushNamedAndRemoveUntil(
-                  notesRoute,
-                  (_) => false,
-                );
+                final user = FirebaseAuth.instance.currentUser;
+                if (user?.emailVerified ?? false) {
+                  // user's Email is verified
+                  Navigator.of(context).pushNamedAndRemoveUntil(
+                    notesRoute,
+                    (_) => false,
+                  );
+                } else {
+                  // user's Email is verified
+                  Navigator.of(context).pushNamedAndRemoveUntil(
+                    verifyEmailRoute,
+                    (_) => false,
+                  );
+                }
               } on FirebaseAuthException catch (e) {
                 if (e.code == 'network-request-failed') {
                   await showErrorDialog(
