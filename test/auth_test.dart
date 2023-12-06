934c1defed91300cb6b82be45451fc3433724a69
Ankit kumar <iamankitm05@gmail.com>
2023-12-06 13:14:25 +0530

    step-18

--- test/auth_test.dart
+++ test/auth_test.dart
@@ -116,6 +116,7 @@ class MockAuthProvider implements AuthProvider {
     if (email == 'foo@bar.com') throw UserNotFoundAuthException();
     if (password == 'foobar') throw WrongPasswordAuthException();
     const user = AuthUser(
+      id: 'my_id',
       isEmailVerified: true,
       email: 'foo@bar.com',
     );
@@ -136,7 +137,11 @@ class MockAuthProvider implements AuthProvider {
     if (!isInitialized) throw NotInitilizeException();
     final user = _user;
     if (user == null) throw UserNotFoundAuthException();
-    const newUser = AuthUser(isEmailVerified: true, email: 'foo@bar.com');
+    const newUser = AuthUser(
+      id: 'my_id',
+      isEmailVerified: true,
+      email: 'foo@bar.com',
+    );
     _user = newUser;
   }
 }
